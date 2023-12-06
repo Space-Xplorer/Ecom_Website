1ca58e2da4ac7947cae6e02c9b16a33345470e48
Nalla Sai Krishna Reddy <105940056+sakrin18072@users.noreply.github.com>
2023-12-06 18:53:08 +0530

    design updated

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -1,86 +1,83 @@
 import axios from "axios";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import React, { useState } from "react";
 import Layout from "./../../components/Layout/Layout";
 
-
 function ForgotPassword() {
-    const [email, setEmail] = useState("");
-    const [newPassword, setnewPassword] = useState("");
-    const [answer, setAnswer] = useState("");
-    const navigate = useNavigate();
-   // const [auth,setAuth]=useAuth()
-   // const location=useLocation();
-    const handleSubmit =async (e) => {
-        e.preventDefault();
-       try{
-          const res=await axios.post(`/api/v1/auth/forgot-password`,{
-            email,
-            newPassword,
-            answer
-        });
-          if(res && res.data.success){
-            toast.success(res.data && res.data.message)
-           
-            navigate("/login");  
-          }else{
-            toast.error(res.data.message)
-          }
-    
-          }
-          catch(error){
-            toast.error("Something Went Wrong")
-           }
-       }
+  const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [answer, setAnswer] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`/api/v1/auth/forgot-password`, {
+        email,
+        newPassword,
+        answer,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      toast.error("Something Went Wrong");
+    }
+  };
 
   return (
-    <Layout title={'Forgot-Password'}>
-        {/* <h1>Forgot Password</h1> */}
-        <div className="form-container" style={{ minHeight: "90vh" }}>
-        <form onSubmit={handleSubmit}>
-          <h4 className="title">Reset Password</h4>
-          <div className="mb-3">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Email "
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={answer}
-              onChange={(e) => setAnswer(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your favourite Sport Name "
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              value={newPassword}
-              onChange={(e) => setnewPassword(e.target.value)}
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter Your  New Password"
-              required
-            />
-          </div>
-        
-          <button type="submit" className="btn btn-primary">
-            Reset
-          </button>
-        </form>
+    <Layout title={"Forgot-Password"}>
+      <div className="container mx-auto p-4">
+        <div className="max-w-md mx-auto bg-white rounded-lg p-6 shadow-lg">
+          <h2 className="text-2xl font-semibold mb-4 text-center text-indigo-600">
+            Reset Password
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-lg"
+                placeholder="Enter Your Email"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-lg"
+                placeholder="Enter Your favorite Sport Name"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-lg"
+                placeholder="Enter Your New Password"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700"
+            >
+              Reset
+            </button>
+          </form>
+        </div>
       </div>
     </Layout>
-  )
+  );
 }
 
-export default ForgotPassword
+export default ForgotPassword;
